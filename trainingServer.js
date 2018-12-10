610b2b8c6efad5a77a9c1d5cdc5889ac4748ee11
Dylan MacKenzie <dmackenz@mail.uoguelph.ca>
2018-12-10 00:09:35 -0500

    Changed training server to get ready for training.

--- trainingServer.js
+++ trainingServer.js
@@ -1,6 +1,1 @@
-const express = require('express');
-
-// Server configuration.
-let app = express();
-app.use(express.static('training'));
-app.listen(3000);
+// Write training script with tf.js here using training data...
